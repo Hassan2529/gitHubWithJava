bb48cefeeef28824b3883cb30c4376d0714ec0f7
M Hassan Raza <muhammadhassanraza015@outlook.com>
2022-06-14 13:29:39 +0500

    This is second commit for Main file and sum code

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args){
         int a = 2;
         int b = 3;
-        System.out.println(a+b);
+        System.out.println("THE SUM IS: "+a+b);
     }
 }
